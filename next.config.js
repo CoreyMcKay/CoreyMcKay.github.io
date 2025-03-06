ddfa739001875b5bdffcb4cbb2f843ea7e1651e4
CoreyMcKay <coreymckay11@gmail.com>
2025-03-05 23:47:13 -0500

    Fix build configuration and remove custom webpack config

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/CoreyMcKay.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/CoreyMcKay.github.io' : '',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig 
