f614bde7bf42a6419d2054fa2c99a5b25a0045c9
CoreyMcKay <coreymckay11@gmail.com>
2025-03-12 18:55:03 -0400

    Remove config files in favor of direct password hashing

--- admin/config.template.js
+++
@@ -1,8 +0,0 @@
-// Template configuration file
-// 1. Copy this file and rename it to config.js
-// 2. Change the ADMIN_PASSWORD to your secure password
-// 3. Keep config.js out of Git (it's in .gitignore)
-
-const CONFIG = {
-    ADMIN_PASSWORD: 'CHANGE_THIS_PASSWORD'
-}; 
